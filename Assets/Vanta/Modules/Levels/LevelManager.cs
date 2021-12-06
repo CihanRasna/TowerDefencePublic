9679ecf33d3b0a6606a9b365476d840fdc07cd19
Cihan Rasna <cihan.rasna@vanta.games>
2021-12-07 01:04:45 +0300

    AnalyticsManager deleted

---
+++ Assets/Vanta/Modules/Levels/LevelManager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Vanta.Core;
+using Vanta.Persist;
+using Random = UnityEngine.Random;
+
+
+
+namespace Vanta.Levels
+{
+
+    public class LevelManager : Singleton<LevelManager>, ILevelDelegate
+    {
+
+        public List<LevelInfo> levels => _levels;
+        [HideInInspector] [SerializeField] private List<LevelInfo> _levels;
+
+        public BaseLevel currentLevel => _currentLevel;
+        private BaseLevel _currentLevel;
+
+        [SerializeField] private bool forceLevel = false;
+        [SerializeField] private int forceLevelIdx = -1;
+        [SerializeField] private bool forceSameLevel = false;
+
+        public GameObject defaultEnvironment;
+        private GameObject currentEnvironment;
+
+        // Events
+        public delegate void LevelEvent(BaseLevel baseLevel);
+        public delegate void LevelEvent1i(BaseLevel baseLevel, int arg1);
+
+        public event LevelEvent levelDidLoad;
+        public event LevelEvent levelDidStart;
+        public event LevelEvent levelDidSuccees;
+        public event LevelEvent levelDidFail;
+        public event LevelEvent levelWillUnload;
+
+
+
+    #region Life Cycle
+        
+        private void Start()
+        {
+            int lastPlayedLevelIdx = PersistManager.Instance.currentLevelIdx;
+
+#if UNITY_EDITOR
+            if (forceLevel)
+            {
+                lastPlayedLevelIdx = forceLevelIdx;
+            }
+#endif
+
+            LoadLevel(lastPlayedLevelIdx);
+        }
+
+    #endregion
+        
+
+
+    #region Loading
+
+        public void LoadLevel(LevelInfo levelInfo)
+        {
+            var levelIdx = levels.IndexOf(levelInfo);
+            LoadLevel(levelIdx);
+        }
+
+        public void LoadLevel(int levelIdx)
+        {
+            var totalLevelCount = levels.Count;
+            
+            if (levelIdx >= totalLevelCount)
+            {
+                levelIdx = Random.Range(0, totalLevelCount);
+            }
+
+            UnloadCurrentLevel();
+
+            var levelInfo = levels[levelIdx];
+            var currentLevelObj = Instantiate(levelInfo.prefab);
+            var level = currentLevelObj.GetComponent<BaseLevel>();
+            level.listener = this;
+            _currentLevel = level;
+
+            if (defaultEnvironment != null)
+            {
+                currentEnvironment = Instantiate(defaultEnvironment);   
+            }
+
+            PersistManager.Instance.currentLevelIdx = levelIdx;
+        }
+
+        private void UnloadCurrentLevel()
+        {
+            if (_currentLevel != null)
+            {
+                levelWillUnload?.Invoke(_currentLevel);
+                Destroy(_currentLevel.gameObject);
+
+                if (currentEnvironment != null)
+                {
+                    Destroy(currentEnvironment);
+                    currentEnvironment = null;
+                }
+            }
+        }
+
+        public void RestartCurrentLevel()
+        {
+            LoadLevel(PersistManager.Instance.currentLevelIdx);
+        }
+
+        public void LoadNextLevel()
+        {
+            PersistManager.Instance.displayingLevelIdx++;
+         
+            bool isUnityEditor = false;
+#if UNITY_EDITOR
+            isUnityEditor = true;
+#endif
+            if (!isUnityEditor || !forceSameLevel)
+            {
+                PersistManager.Instance.currentLevelIdx++;
+            }
+         
+            LoadLevel(PersistManager.Instance.currentLevelIdx);
+        }
+
+    #endregion
+
+
+
+    #region Level Delegate
+
+        public void Level_DidLoad(BaseLevel baseLevel) => levelDidLoad?.Invoke(_currentLevel);
+        public void Level_DidStart(BaseLevel baseLevel) => levelDidStart?.Invoke(_currentLevel);
+        public void Level_DidSuccess(BaseLevel baseLevel, float score) => levelDidSuccees.Invoke(_currentLevel);
+        public void Level_DidFail(BaseLevel baseLevel) => levelDidFail?.Invoke(_currentLevel);
+
+    #endregion
+
+    }
+
+}
---
+++ Assets/Vanta/Modules/UI/MenuManager.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using Vanta.Core;
+using Vanta.Levels;
+using Vanta.Persist;
+
+
+
+namespace Vanta.UI
+{
+    public class MenuManager : Singleton<MenuManager>, 
+        IGamePanelDelegate, 
+        ISettingsPanelDelegate, 
+        ILevelFailedPanelDelegate, 
+        ILevelSucceededPanelDelegate,
+        IEditorPanelDelegate
+    {
+        
+        [SerializeField] private GamePanel _gamePanel;
+        [SerializeField] private SettingsPanel _settingsPanel;
+        [SerializeField] private LevelFailedPanel _levelFailedPanel;
+        [SerializeField] private LevelSucceededPanel _levelSucceededPanel;
+        [SerializeField] private TutorialPanel _tutorialPanel;
+        [SerializeField] private EditorPanel _editorPanel;
+        
+        [SerializeField] [HideInInspector] private SafeArea.SimulationDevice _simulatingDevice = SafeArea.SimulationDevice.None;
+
+
+    
+    #region Life Cycle
+    
+        protected override void Awake()
+        {
+            base.Awake();
+            
+#if UNITY_EDITOR
+            SafeArea.simulationDevice = _simulatingDevice;
+#endif
+        }
+
+        private void Start()
+        {
+            _gamePanel.listener = this;
+            _settingsPanel.listener = this;
+            _levelFailedPanel.listener = this;
+            _levelSucceededPanel.listener = this;
+            _editorPanel.listener = this;
+
+            LevelManager.Instance.levelDidLoad += LevelDidLoad;
+            LevelManager.Instance.levelDidStart += LevelDidStart;
+            LevelManager.Instance.levelDidSuccees += LevelDidSuccess;
+            LevelManager.Instance.levelDidFail += LevelDidFail;
+        }
+
+    #endregion
+
+
+
+    #region Level Events
+
+        private void LevelDidLoad(BaseLevel baseLevel)
+        {
+            HideAllPanels();
+        
+            _gamePanel.UpdateLevelIndex(PersistManager.Instance.displayingLevelIdx);
+            _gamePanel.UpdateProgressBar(0, false);
+            _gamePanel.Display();
+        
+            _tutorialPanel.Display();
+        }
+
+        private void LevelDidStart(BaseLevel baseLevel)
+        {
+            _tutorialPanel.Hide();
+        }
+
+        private void LevelDidSuccess(BaseLevel baseLevel)
+        {
+            HideAllPanels();
+            _levelSucceededPanel.Display();
+        }
+
+        private void LevelDidFail(BaseLevel baseLevel)
+        {
+            HideAllPanels();
+            _levelFailedPanel.Display();
+        }
+
+    #endregion
+    
+    
+    
+    #region User Interaction Logic
+
+        public void GamePanel_SettingsButtonTapped(GamePanel gamePanel)
+        {
+            GameManager.Instance.PauseGame();
+            _gamePanel.DisplaySettingsButton(false);
+            _tutorialPanel.Hide();
+            _settingsPanel.Display();
+        }
+
+        public void GamePanel_EditorButtonTapped(GamePanel gamePanel)
+        {
+            _editorPanel.Display();
+        }
+
+        public void SettingsPanel_CloseButtonTapped(SettingsPanel settingsPanel)
+        {
+            GameManager.Instance.ResumeGame();
+            _settingsPanel.Hide();
+            _gamePanel.DisplaySettingsButton(true);
+            if (LevelManager.Instance.currentLevel.state == BaseLevel.State.Unknown ||
+                LevelManager.Instance.currentLevel.state == BaseLevel.State.Loading ||
+                LevelManager.Instance.currentLevel.state == BaseLevel.State.Loaded)
+            {
+                _tutorialPanel.Display();
+            }
+        }
+
+        public void LevelFailedPanel_TryAgainButtonTapped(LevelFailedPanel levelFailedPanel)
+        {
+            LevelManager.Instance.RestartCurrentLevel();
+        }
+
+        public void LevelSucceededPanel_NextButtonTapped(LevelSucceededPanel levelSucceededPanel)
+        {
+            LevelManager.Instance.LoadNextLevel();
+        }
+
+        public void EditorPanel_LoadLevelButtonTapped(EditorPanel panel, int levelIdx)
+        {
+            LevelManager.Instance.LoadLevel(levelIdx);
+        }
+
+        public void EditorPanel_CloseButtonTapped(EditorPanel panel)
+        {
+            _editorPanel.Hide();
+        }
+
+    #endregion
+    
+    
+    
+    #region Private Methods
+
+        private void HideAllPanels()
+        {
+            _gamePanel.Hide();
+            _settingsPanel.Hide();
+            _levelSucceededPanel.Hide();
+            _levelFailedPanel.Hide();
+            _tutorialPanel.Hide();
+            _editorPanel.Hide();
+        }
+    
+    #endregion
+
+    }
+
+}
