f977f19ee2bbcabca5d2c73f59cdb084be887f6b
Cihan Rasna <cihan.rasna@vanta.games>
2021-12-08 18:02:29 +0300

    Status effect scriptable object editor with Odin

--- Assets/_Game/Scripts/ScriptableProperties/StatusEffects.cs
+++ Assets/_Game/Scripts/ScriptableProperties/StatusEffects.cs
@@ -1,9 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "StatusEffects", menuName = "ScriptableObjects/StatusEffectScriptableObject", order = 3)]
 public class StatusEffects : ScriptableObject
 {
+    [TitleGroup("Ice Tower", alignment: TitleAlignments.Centered)]
+    [HorizontalGroup("Ice Tower/Split")]
+    [VerticalGroup("Ice Tower/Split/Left")]
+    [BoxGroup("Ice Tower/Split/Left/Freeze Time"),HideLabel]
+    public int freezeTime;
     
+    [VerticalGroup("Ice Tower/Split/Right")] 
+    [BoxGroup("Ice Tower/Split/Right/Freeze Ratio"),HideLabel]
+    public float freezeRatio;
+    
+    [TitleGroup("Fire Tower", alignment: TitleAlignments.Centered)]
+    [HorizontalGroup("Fire Tower/Split")]
+    [VerticalGroup("Fire Tower/Split/Left")]
+    [BoxGroup("Fire Tower/Split/Left/Burn Time"),HideLabel]
+    public int burnTime;
+    
+    [VerticalGroup("Fire Tower/Split/Right")] 
+    [BoxGroup("Fire Tower/Split/Right/Burn Ratio"),HideLabel]
+    public float burnRatio;
+    
+    [TitleGroup("Magic Tower", alignment: TitleAlignments.Centered)]
+    [HorizontalGroup("Magic Tower/Split")]
+    [VerticalGroup("Magic Tower/Split/Left")]
+    [BoxGroup("Magic Tower/Split/Left/Poison Time"),HideLabel]
+    public int poisonTime;
+    
+    [VerticalGroup("Magic Tower/Split/Right")] 
+    [BoxGroup("Magic Tower/Split/Right/Poison Ratio"),HideLabel]
+    public float poisonRatio;
+    
+    [TitleGroup("Teleport Tower", alignment: TitleAlignments.Centered)]
+    [BoxGroup("Teleport Tower/Teleport Place", true,true),HideLabel]
+    public int teleportPlace;
 }
--- Assets/_Game/Scripts/ScriptableProperties/TowerProperties.cs
+++ Assets/_Game/Scripts/ScriptableProperties/TowerProperties.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "TowerProperties", menuName = "ScriptableObjects/TowerPropertiesScriptableObject", order = 2)]
+[CreateAssetMenu(fileName = "TowerProperties", menuName = "ScriptableObjects/TowerPropertiesScriptableObject",
+    order = 2)]
 public class TowerProperties : ScriptableObject
 {
+    [Title("Tower Type", titleAlignment: TitleAlignments.Centered), EnumToggleButtons, HideLabel]
     public BaseTower.Type towerType;
+    [Space(20)]
+    
+    [Title("Projectile",titleAlignment:TitleAlignments.Centered)]
+    [HideLabel,PreviewField(100, ObjectFieldAlignment.Center)]
     public Projectile projectile;
     public float bulletSpeed;
     public float damage;
