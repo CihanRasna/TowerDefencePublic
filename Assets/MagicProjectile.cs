d9a0cb8db981f13ce6d19982bc21c5b0fbeeaaf5
Cihan Rasna <cihan.rasna@vanta.games>
2022-05-16 22:32:02 +0300

    Magic tower projectile setting*

---
+++ Assets/MagicProjectile.cs
@@ -0,0 +1,15 @@
+using _Game.Levels.Base;
+using _Game.Scripts.Enemy;
+using _Game.Scripts.Projectiles;
+using Vanta.Levels;
+
+public class MagicProjectile : BaseProjectile
+{
+
+    protected override void DoYourOwnShit(BaseEnemy baseEnemy)
+    {
+        var level = LevelManager.Instance.currentLevel as Level;
+        Destroy(Instantiate(hitParticle, transform.position, transform.rotation, level.transform), 2f);
+        Destroy(gameObject);
+    }
+}
